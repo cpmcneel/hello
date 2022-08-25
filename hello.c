e3c0c25ee2699f19c5ba7561b9482f37810852c1
Connor McNeel <cmcneel2@huskers.unl.edu>
2022-08-25 17:51:38 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Connor McNeel
+ * 2022/08/25
+ *
+ * A program that prints my name in c
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+    int main(int argc, char **argv) {
+
+    printf("Connor McNeel\n");
+
+    return 0;
+}
